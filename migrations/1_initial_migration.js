d3d265d768afc6fd9a0e82f3b1ce1c5e080d099f
Dennis von der Bey <dennis@vonderbey.eu>
2022-08-29 22:53:49 +0200

    chore: remove transpiled files

--- migrations/1_initial_migration.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Migrations = artifacts.require('Migrations');
-const migration = function (deployer) {
-    deployer.deploy(Migrations);
-};
-module.exports = migration;
-//# sourceMappingURL=1_initial_migration.js.map
--- migrations/2_deploy_contract.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const RevocationRegistry = artifacts.require('RevocationRegistry');
-const migration = function (deployer) {
-    deployer.deploy(RevocationRegistry);
-};
-module.exports = migration;
-//# sourceMappingURL=2_deploy_contract.js.map
