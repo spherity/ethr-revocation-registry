9bb90628de198be34e14539e0968ef332b368559
Dennis von der Bey <dennis@vonderbey.eu>
2022-08-31 17:33:32 +0200

    chore: wip on signed methods

--- contracts/RevocationRegistry.sol
+++ contracts/RevocationRegistry.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity >=0.7.0 <0.9.0;
+pragma solidity >=0.8.11 <0.9.0;
 
 import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
-contract RevocationRegistry is EIP712, ChangeStatus {
+contract RevocationRegistry is EIP712 {
     // Revocations happen in revocation lists that belong to an address/ user namespace
     mapping(address => mapping(bytes32 => mapping(bytes32 => bool))) registry;
 
@@ -21,8 +21,17 @@ contract RevocationRegistry is EIP712, ChangeStatus {
     // Nonce tracking for meta transactions
     mapping(address => uint) nonces;
 
+    string public constant VERSION_MAJOR = "1";
+    string public constant VERSION_MINOR = "0";
+    string public constant VERSION_PATCH = "0";
+    string constant VERSION_DELIMITER = ".";
+
     constructor()
-    EIP712("Revocation-Registry", "1.0.0") {}
+    EIP712("Revocation Registry", version()) {}
+
+    function version() public pure returns(string memory) {
+        return string.concat(VERSION_MAJOR, VERSION_DELIMITER, VERSION_MINOR, VERSION_DELIMITER, VERSION_PATCH);
+    }
 
     function isRevoked(address namespace, bytes32 list, bytes32 revocationKey) public view returns (bool) {
         return (registry[namespace][list][revocationKey]);
@@ -37,21 +46,22 @@ contract RevocationRegistry is EIP712, ChangeStatus {
         // emit Event
     }
 
+    // TODO: rename `signed` suffix to something more self explanatory? Like `meta` maybe?
     function changeStatusSigned(bool revoked, address namespace, bytes32 list, bytes32 revocationKey, bytes calldata signature) public {
         bytes32 hash = _hashChangeStatus(revoked, namespace, list, revocationKey);
         address signer = ECDSA.recover(hash, signature);
-        require(identityIsOwner(namespace, list, signer), "Signer is not an owner");
+        require(_identityIsOwner(namespace, list, signer), "Signer is not an owner");
         nonces[signer]++;
         _changeStatus(revoked, namespace, list, revocationKey);
     }
 
-    function _hashChangeStatus(bool revoked, address namespace, bytes32 list, bytes32 revocationKey) public returns(bytes32) {
+    function _hashChangeStatus(bool revoked, address namespace, bytes32 list, bytes32 revocationKey) view public returns(bytes32) {
         return _hashTypedDataV4(keccak256(abi.encode(
-                keccak256("ChangeStatus(bool revoked,address namespace,bytes32 list,bytes32 key)"),
+                keccak256("ChangeStatus(bool revoked,address namespace,bytes32 list,bytes32 revocationKey)"),
                 revoked,
                 namespace,
                 list,
-                key
+                revocationKey
             )));
     }
 
@@ -85,9 +95,6 @@ contract RevocationRegistry is EIP712, ChangeStatus {
     function generateListLocationHash(address namespace, bytes32 list) pure internal returns(bytes32) {
         return keccak256(abi.encodePacked(namespace, list));
     }
-    function generateListLocationVersionHash(address namespace, bytes32 list, uint version) pure internal returns(bytes32) {
-        return keccak256(abi.encodePacked(namespace, list, version));
-    }
 
     modifier isOwner(address namespace, bytes32 list) {
         require(_identityIsOwner(namespace, list, msg.sender), "Caller is not an owner");
@@ -97,7 +104,7 @@ contract RevocationRegistry is EIP712, ChangeStatus {
     // Check if:
     // - identity that is supplied is acting in its namespace
     // - or they got owner rights in a foreign namespace
-    function _identityIsOwner(address namespace, bytes32 list, address identity) pure internal returns(bool) {
+    function _identityIsOwner(address namespace, bytes32 list, address identity) view internal returns(bool) {
         bytes32 listLocationHash = generateListLocationHash(namespace, list);
         if (!newOwners[listLocationHash][identity]) {
             return false;
@@ -111,7 +118,7 @@ contract RevocationRegistry is EIP712, ChangeStatus {
     }
 
     // Check if caller got delegate rights in a foreign namespace before expiry
-    function _identityIsDelegate(address namespace, bytes32 list, address identity) pure internal returns(bool) {
+    function _identityIsDelegate(address namespace, bytes32 list, address identity) view internal returns(bool) {
         bytes32 listLocationHash = generateListLocationHash(namespace, list);
         if(delegates[listLocationHash][identity] > block.timestamp) {
             return true;
