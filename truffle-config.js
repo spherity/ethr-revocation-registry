498b282a75ffb1c86bd879e5f9956c785e58049a
Dennis von der Bey <Dennis@vonderbey.eu>
2022-09-13 12:07:15 +0200

    feat: add build pipeline (#24)

--- truffle-config.js
+++ truffle-config.js
@@ -67,11 +67,6 @@ module.exports = {
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     //
-    development: {
-      host: "127.0.0.1",     // Localhost (default: none)
-      port: 8545,            // Standard Ethereum port (default: none)
-      network_id: "5777",       // Any network (default: none)
-    },
     //
     // An additional network, but with some advanced options…
     // advanced: {
