9bec1f353828048cd3da14cc6ae7856fd13c05bd
Dennis von der Bey <dennis@vonderbey.eu>
2022-09-15 13:02:44 +0200

    feat: add EIP712 type for changeStatusDelegated

--- src/util.ts
+++ src/util.ts
@@ -10,3 +10,14 @@ export const EIP712ChangeStatusType = {
         {name: 'nonce', type: 'uint'} as TypedDataField,
     ]
 }
+
+export const EIP712ChangeStatusDelegatedType = {
+    ChangeStatusDelegated: [
+        {name: 'revoked', type: 'bool'} as TypedDataField,
+        {name: 'namespace', type: 'address'} as TypedDataField,
+        {name: 'revocationList', type: 'bytes32'} as TypedDataField,
+        {name: 'revocationKey', type: 'bytes32'} as TypedDataField,
+        {name: 'signer', type: 'address'} as TypedDataField,
+        {name: 'nonce', type: 'uint'} as TypedDataField,
+    ]
+}
