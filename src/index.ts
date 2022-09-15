f48bb5f2cd1b39eba971e01fafc3a8ee6d3e1108
Dennis von der Bey <dennis@vonderbey.eu>
2022-09-15 13:08:33 +0200

    feat: add all other EIP712 types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
 export {
-    EIP712ChangeStatusType
+    EIP712ChangeStatusType,
+    EIP712ChangeStatusDelegatedType,
+    EIP712ChangeStatusInListType,
+    EIP712ChangeStatusesInListDelegatedType,
+    EIP712ChangeListOwnerType,
+    EIP712AddListDelegateType,
+    EIP712RemoveListDelegateType,
+    EIP712ChangeListStatusType,
 } from "./util"
--- src/util.ts
+++ src/util.ts
@@ -21,3 +21,66 @@ export const EIP712ChangeStatusDelegatedType = {
         {name: 'nonce', type: 'uint'} as TypedDataField,
     ]
 }
+
+export const EIP712ChangeStatusInListType = {
+    ChangeStatusesInList: [
+        {name: 'revoked', type: 'bool[]'},
+        {name: 'namespace', type: 'address'},
+        {name: 'revocationList', type: 'bytes32'},
+        {name: 'revocationKeys', type: 'bytes32[]'},
+        {name: 'signer', type: 'address'},
+        {name: 'nonce', type: 'uint'},
+    ]
+}
+
+export const EIP712ChangeStatusesInListDelegatedType = {
+    ChangeStatusesInListDelegated: [
+        {name: 'revoked', type: 'bool[]'},
+        {name: 'namespace', type: 'address'},
+        {name: 'revocationList', type: 'bytes32'},
+        {name: 'revocationKeys', type: 'bytes32[]'},
+        {name: 'signer', type: 'address'},
+        {name: 'nonce', type: 'uint'},
+    ]
+}
+
+export const EIP712ChangeListOwnerType = {
+    ChangeListOwner: [
+        {name: 'namespace', type: 'address'},
+        {name: 'newOwner', type: 'address'},
+        {name: 'revocationList', type: 'bytes32'},
+        {name: 'signer', type: 'address'},
+        {name: 'nonce', type: 'uint'},
+    ]
+}
+
+export const EIP712AddListDelegateType = {
+    AddListDelegate: [
+        {name: 'namespace', type: 'address'},
+        {name: 'delegate', type: 'address'},
+        {name: 'revocationList', type: 'bytes32'},
+        {name: 'validity', type: 'uint'},
+        {name: 'signer', type: 'address'},
+        {name: 'nonce', type: 'uint'},
+    ]
+}
+
+export const EIP712RemoveListDelegateType = {
+    RemoveListDelegate: [
+        {name: 'namespace', type: 'address'},
+        {name: 'delegate', type: 'address'},
+        {name: 'revocationList', type: 'bytes32'},
+        {name: 'signer', type: 'address'},
+        {name: 'nonce', type: 'uint'},
+    ]
+}
+
+export const EIP712ChangeListStatusType = {
+    ChangeListStatus: [
+        {name: 'revoked', type: 'bool'},
+        {name: 'namespace', type: 'address'},
+        {name: 'revocationList', type: 'bytes32'},
+        {name: 'signer', type: 'address'},
+        {name: 'nonce', type: 'uint'},
+    ]
+}
